7e20e67982d5eba652d73c39bbc50279261709cc
Programmer Telo <programertelo@gmail.com>
2024-08-14 12:47:51 +0700

    feat: adding seeder

--- database/migrations/2024_07_18_013622_create_countries_table.php
+++ database/migrations/2024_07_18_013622_create_countries_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('code', 2);
+            $table->smallInteger('phonecode');
             $table->timestamps();
         });
     }
