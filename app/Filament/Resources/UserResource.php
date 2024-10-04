3ef84114cde3a3108e7693dbc39b8c770df20ce1
Programmer Telo <programertelo@gmail.com>
2024-10-04 10:35:51 +0700

    adding role and permission

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -31,7 +32,8 @@ class UserResource extends Resource
                     ->unique(ignoreRecord: true)
                     ->required()
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Select::make('roles')
+                    ->relationship('roles', 'name'),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
@@ -47,9 +49,8 @@ class UserResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('email_verified_at')
-                    ->dateTime()
-                    ->sortable(),
+                Tables\Columns\TextColumn::make('roles.name')
+                    ->formatStateUsing(fn($state): string => str()->headline($state)),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
